42ff429ad45b7ed507f4022b022361cb11249bae
siraku <slstc6666@yahoo.co.jp>
2022-10-31 12:26:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Aa(){
+  return (
+    <div>
+      may I ask you a question
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit aaaaa <code>src/App.js</code>save to reload.
         </p>
         <a
           className="App-link"
@@ -15,8 +23,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!
         </a>
+      <Aa/>
       </header>
     </div>
   );
